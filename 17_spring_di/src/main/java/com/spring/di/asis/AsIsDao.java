2b9c5a166968363d4e39c52c5c34afb4096e0187
User <User@DESKTOP-OIHGT6P>
2022-07-29 17:43:36 +0900

    의존성 주입 + IoC

---
+++ 17_spring_di/src/main/java/com/spring/di/asis/AsIsDao.java
@@ -0,0 +1,10 @@
+package com.spring.di.asis;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AsIsDao {
+	
+	
+	
+}
---
+++ 17_spring_di/src/main/java/com/spring/di/tobe/ToBeDao.java
@@ -0,0 +1,8 @@
+package com.spring.di.tobe;
+
+public interface ToBeDao {
+	
+	
+	
+	
+}
